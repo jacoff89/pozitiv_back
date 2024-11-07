4e0574d11f836c6bcea7032b687a65593833e5d2
Yakov <jacoff8920@mail.ru>
2024-11-07 17:15:26 +0500

    Мелкие правки

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ApiController;
-use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-//Route::get('api/v1/reviews', [ApiController::class, 'reviews'])->name('reviews');
-//Route::match(['get', 'post'],'wp-json/pozitiv/v1/user/current', [ApiController::class, 'tours'])->name('test1');
